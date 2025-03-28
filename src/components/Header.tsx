23b86139e3fe43a3aa3617ecb8f98d4323c4b88b
Arthur Henryck <thuy.henryck@gmail.com>
2025-03-28 11:32:38 -0300

    components

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <>
+            <div className="header">{/*Cabeçalho*/}
+                <img src="c:\Users\Estudante\Downloads\icons8-computador-64.png" className="logo" />
+                <p className="cyber"> Cyber Assist </p>
+            </div>
+        </>
+    )
+}
